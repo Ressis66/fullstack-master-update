3b56629dde22a3c8062933e1f344e8b5c1856c21
Ressis66 <vaganov.aleksandr.88@mail.ru>
2024-04-08 16:54:39 +0500

    Initial commit

---
+++ src/main/java/com/alexwork/fullstack/FullstackApplication.java
@@ -0,0 +1,16 @@
+package com.alexwork.fullstack;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackApplication.class, args);
+	}
+
+
+}
